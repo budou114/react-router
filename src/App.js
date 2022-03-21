dc3ef9748d76b7c6be70e2f931073f52dbb9abb4
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-21 15:04:03 +0900

    遷移用のlinkタグを追加

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Link } from 'react-router-dom';
+
 import { Home } from './Home';
 import { Page1 } from './Page1';
 import { Page2 } from './Page2';
@@ -5,10 +7,17 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className='App'>
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/Page1'>Page1</Link>
+        <br />
+        <Link to='/Page2'>Page2</Link>
+        <Home />
+        <Page1 />
+        <Page2 />
+      </div>
+    </BrowserRouter>
   );
 };
